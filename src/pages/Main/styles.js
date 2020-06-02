5f6ecaaf716092e88891cce355a93900c6635cd6
Gosch <dev@gosch.io>
2020-06-02 14:33:37 -0300

    Implements Wrapper to PokemonList

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,10 +1,12 @@
 import styled from 'styled-components';
 
+import { Wrapper } from 'visual/styles/Wrapper';
+
 export const Container = styled.div`
   
 `;
 
-export const PokemonList = styled.div`
+export const PokemonList = styled(Wrapper)`
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
